902c5edfdebe2572677b0157f7cd4ad978a47b1b
yajamon <yajamon.tatsuki@gmail.com>
2020-03-20 20:02:40 +0900

    BucketsにDisplayを実装

--- src/buckets.rs
+++ src/buckets.rs
@@ -1,9 +1,23 @@
-#[derive(Debug)]
+use std::fmt;
+
+#[derive(Debug, Clone)]
 pub struct Buckets(pub i64);
+impl fmt::Display for Buckets {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mb: MilliBuckets = self.clone().into();
+        mb.fmt(f)
+    }
+}
 
 #[derive(Debug)]
 pub struct MilliBuckets(pub i64);
 pub type MB = MilliBuckets;
+impl fmt::Display for MilliBuckets {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let MilliBuckets(value) = self;
+        write!(f, "{} mB", value)
+    }
+}
 
 impl From<Buckets> for MilliBuckets {
     fn from(buckets: Buckets) -> Self {
